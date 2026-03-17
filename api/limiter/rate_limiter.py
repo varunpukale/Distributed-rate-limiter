ab0201de14a1096af13245f58d9b9192e439b6f5
Varun Pukale <varun.pukale.018@gmail.com>
2026-03-16 21:43:26 -0400

    Add rate limiter core logic using Redis

---
+++ api/limiter/rate_limiter.py
@@ -0,0 +1,24 @@
+import time
+from utils.redis_client import redis_client
+
+class RateLimiter:
+    def __init__(self, limit=5, window=60):
+        self.limit = limit
+        self.window = window
+
+    def is_allowed(self, user_id):
+        key = f"rate_limit:{user_id}"
+        current_time = int(time.time())
+
+        # Increment request count
+        count = redis_client.incr(key)
+
+        # Set expiry for new key
+        if count == 1:
+            redis_client.expire(key, self.window)
+
+        # Check limit
+        if count > self.limit:
+            return False
+        
+        return True
